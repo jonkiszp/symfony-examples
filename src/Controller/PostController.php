7c9b65b4e16355deea0f61a113d902aaa5054c8c
Paweł <jonkiszp@gmail.com>
2026-04-20 11:23:37 +0200

    feat: implement post entity, migration, controller, and list view with database port exposure

---
+++ src/Controller/PostController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class PostController extends AbstractController
+{
+    #[Route('/posts', name: 'post_list')]
+    public function list(EntityManagerInterface $entityManager): Response
+    {
+        $posts = $entityManager->getRepository(Post::class)->findAll();
+
+        return $this->render('post/list.html.twig', [
+            'posts' => $posts,
+        ]);
+    }
+
+    #[Route('/post/create', name: 'post_create')]
+    public function create(EntityManagerInterface $entityManager): Response
+    {
+        $post = new Post('Tytuł', 'Treść');
+        $entityManager->persist($post);
+        $entityManager->flush();
+        return $this->redirectToRoute('post_list');
+    }
+}
